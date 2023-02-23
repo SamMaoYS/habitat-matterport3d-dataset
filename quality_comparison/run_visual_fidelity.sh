09ba62bca8e86276d17b9c1aab68c10a8c5bdefc
Yongsen Mao <yma50@sfu.ca>
2023-02-23 15:37:50 -0800

    update quality comparision

--- quality_comparison/run_visual_fidelity.sh
+++ quality_comparison/run_visual_fidelity.sh
@@ -4,15 +4,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-REAL_ROOT="$SAVE_DIR_PATH/real_images"
+REAL_ROOT="$SAVE_DIR_PATH/simulated_images"
 SIM_ROOT="$SAVE_DIR_PATH/simulated_images"
 
 
-# Comparing simulated images with real images
-for real in "mp3d_real" "gibson_real"
+Comparing simulated images with real images
+for real in "hm3d_sim"
 do
-   for sim in "replica_sim" "robothor_sim" "mp3d_sim" "gibson_4_plus_sim" \
-              "gibson_sim" "scannet_sim" "hm3d_sim"
+   for sim in "fp_sim" "procthor_sim"
    do
        echo "=========> Comparing $sim with $real"
        python measure_visual_fidelity.py \
@@ -23,9 +22,9 @@ done
 
 
 # Comparing real images with real images
-for real_1 in "mp3d_real" "gibson_real"
+for real_1 in "hm3d_sim"
 do
-   for real_2 in "mp3d_real" "gibson_real"
+   for real_2 in "hm3d_sim"
    do
        echo "=========> Comparing $real_1 with $real_2"
        python measure_visual_fidelity.py \
