28f20bdac7f5aab771a1818a91f446ad83f31746
Yongsen Mao <sammaoys@gmail.com>
2023-09-01 19:48:46 +0800

    update quality comp

--- quality_comparison/run_real_extraction.sh
+++ quality_comparison/run_real_extraction.sh
@@ -8,5 +8,5 @@ REAL_ROOT="$SAVE_DIR_PATH/real_images"
 
 mkdir "$REAL_ROOT"
 
-python extract_gibson_real.py --dataset-dir "$GIBSON_PANO_ROOT" --save-dir "$REAL_ROOT"/gibson_real
+# python extract_gibson_real.py --dataset-dir "$GIBSON_PANO_ROOT" --save-dir "$REAL_ROOT"/gibson_real
 python extract_mp3d_real.py --dataset-dir "$MP3D_PANO_ROOT" --save-dir "$REAL_ROOT"/mp3d_real
